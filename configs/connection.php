06ede453c4cb450229746e6637a05e31fc1dbbc2
Karthik K <karthikkanumolu123@gmail.com>
2024-05-01 21:00:28 -0700

    Final Commit

---
+++ configs/connection.php
@@ -0,0 +1,40 @@
+<?php
+# -------------------------------------------------#
+#¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤#
+#	¤                                            ¤   #
+#	¤         Puerto Premium Survey 1.0          ¤   #
+#	¤--------------------------------------------¤   #
+#	¤              By Khalid Puerto              ¤   #
+#	¤--------------------------------------------¤   #
+#	¤                                            ¤   #
+#	¤  Facebook : fb.com/prof.puertokhalid       ¤   #
+#	¤  Instagram : instagram.com/khalidpuerto    ¤   #
+#	¤  Site : http://www.puertokhalid.com        ¤   #
+#	¤                                            ¤   #
+#¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤#
+# -------------------------------------------------#
+
+
+$connect = [
+	# Database Host Name
+	'HOSTNAME' => 'localhost',
+
+	# Database Username
+	'USERNAME' => 'u546356589_software',
+
+	# Database Password
+	'PASSWORD' => '85207410sS!',
+
+	# Database Name
+	'DATABASE' => 'u546356589_software'
+];
+
+# Tables' Prefix
+define('prefix', 'puerto_');
+
+# No need to change anything bellow this line
+$db = new mysqli($connect['HOSTNAME'], $connect['USERNAME'], $connect['PASSWORD'], $connect['DATABASE']);
+if($db->connect_errno){
+    echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
+		exit;
+}
